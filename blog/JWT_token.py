5fc823d11bc64b5703e13eeeeb6e0f2d84ea8170
iamaswathisnair <snairaswathi5@gmail.com>
2024-10-16 22:26:10 +0530

    small chnages in jwt

--- blog/JWT_token.py
+++ blog/JWT_token.py
@@ -6,10 +6,11 @@ ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 
-
+# to handle multiple databases in Django.
 def create_access_token(data: dict):
     to_encode = data.copy()
     expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
+   
+    return encoded_jwt 
